9f91056406e3c4c2eba9c47fbcfcd9db2834cc3e
Aakash Banerjee <aakash.banerjee@gmail.com>
2025-05-29 08:50:28 -0700

    lambda files to be used by pipeline created with cloudformation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# devlambdapipelinewcf
-Lambda code and associated files that will be used by codepipeline created with Cloudformation to deploy the lambda
+# DevLambdaPipeline created with Cloudformation module instead of terraform
+
+
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+// Handler handles the CloudWatch Event
+func Handler(ctx context.Context, event events.CloudWatchEvent) {
+
+	log.Println(string(event.Detail))
+
+	_, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatalf("Unable to load SDK config, %v", err)
+	}
+
+}
+
+func main() {
+	lambda.Start(Handler)
+}
